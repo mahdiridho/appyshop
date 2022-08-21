b182099bfe62382576f5dfd588773a4dbb3f0c10
Mahdi <mahdi.ridho@gmail.com>
2022-08-21 18:56:11 +0700

    installation scripts

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo ======
+echo preinstall
+echo
+
+# populating devops
+./initial-setup.js
+
+requirements='@jcshop/jc-checkout'
+yalcDir=`yalc dir`
+
+# deleting old packages
+rm -rf node_modules package-lock.json .yalc yalc.lock
+
+# link local requirements
+for r in $requirements; do
+  if [ ! -d $yalcDir/packages/$r ]; then
+    echo $r
+    zenity --info --text="$r is not already linked to the global $yalcDir/packages. go to $r\'s location and run yalc publish there first"
+    echo
+    break
+  else
+    echo
+    echo $r
+    echo
+    yalc add $r
+  fi
+done
+
+echo
+echo preinstall done
+echo
+echo installing the packages
+npm i
